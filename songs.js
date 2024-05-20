0f289ffd59e1a43653e1b221fb0cfcc2ca400e19
rodrigoaraujo1409 <rodrigoaraujo1409@gmail.com>
2024-05-19 21:47:49 -0300

    primeiro commit

---
+++ songs.js
@@ -0,0 +1,16 @@
+export default [
+    {
+      src: "./src/Peixes.mp3",
+      name: "Peixes - Xamã",
+      cover: "./src/peixes.jpg"
+    },
+    {
+      src: "./src/Pollo-Piritubacity.mp3",
+      name: "Pollo PiriTubaCity",
+
+    },
+    {
+      src: "./src/Touro.mp3",
+      name: "Touro - Xamã",
+    },
+  ];
